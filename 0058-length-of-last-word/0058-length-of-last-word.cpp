ad0b98da0c73149c345a388dc4e985974233a462
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2025-10-26 09:44:41 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.68%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int e = s.size()-1 , cnt = 0;
-        while(s[e] == ' ' && e>0)
+        while(e >= 0 && s[e] == ' ' )
          e--;
 
         while(e >= 0 && s[e] != ' ' )
