c20545376e561b36b87cb11f07da219cd21c0184
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2025-10-28 19:45:13 +0530

    Time: 4 ms (68.35%), Space: 22.2 MB (56.42%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> prefix(n + 1, 0);
-        for (int i = 0; i < nums.size(); i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
+        vector<int> curr ;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] == 0)
+             curr.push_back(i);
+        }
+        int lhsSum =0 , rhsSum=0, j=0, ans=0;
+        while(j<curr.size())
+        {
+            for(int i=0; i<curr[j];i++)
+        {
+            lhsSum += nums[i];
+        }
+        for(int i=curr[j]+1; i<nums.size();i++)
+        {
+            rhsSum += nums[i];
+        }
+        if(lhsSum == rhsSum)
+         ans += 2;
+
+       else if (abs(lhsSum - rhsSum) == 1)
+         ans += 1; 
+
+         j++;
+         lhsSum =0, rhsSum =0;
         }
 
-        int totalsum = prefix[n], ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                int left = prefix[i];
-                int right = totalsum - prefix[i + 1];
-
-                if (left == right)
-                    ans += 2;
-
-                else if (abs(left - right) == 1)
-                    ans += 1;
-            }
-        }
-        return ans;
+ return ans;
     }
 };
