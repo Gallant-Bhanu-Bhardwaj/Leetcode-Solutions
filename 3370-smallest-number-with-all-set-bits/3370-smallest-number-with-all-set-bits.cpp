05a6fd9db5c4cc2dbbf8df45872eb325fee7ca39
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2025-10-30 01:55:47 +0530

    Time: 0 ms (100%), Space: 8.2 MB (89.13%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -1,25 +1,10 @@
-// class Solution {
-// public:
-//     long long smallestNumber(long long n) {
-//         long long x = 1;
-//         while ((x | (x - 1)) < n) { 
-//             x <<= 1;
-//         }
-
-//         long long res = 1;
-//         while (res < n) {
-//             res = (res << 1) | 1;  
-//         }
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
-    long long smallestNumber(long long n) {
-       int k = floor(log2(n));
-       int x = pow(2,k+1) - 1;
-       return x;
+    int smallestNumber(int n) {
+        long long res = 1;
+        while (res < n) {
+            res = (res << 1) | 1;
+        }
+        return res;
     }
 };
-
