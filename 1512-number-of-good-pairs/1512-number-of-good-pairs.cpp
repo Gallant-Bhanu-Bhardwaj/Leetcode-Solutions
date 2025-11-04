3a4f46b1393d626c782ab59bc5f51b3ae369f9d5
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2025-11-04 14:03:28 +0530

    Time: 0 ms (100%), Space: 10 MB (5.37%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int cnt=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i;j<nums.size();j++)
-            {
-                if((nums[i] == nums[j])&( i < j))
-                 cnt++;
-            }
+        unordered_map<int, int> freq;
+        int cnt = 0;
+        for (int x : nums) {
+            cnt += freq[x]; 
+            freq[x]++;
         }
         return cnt;
     }
