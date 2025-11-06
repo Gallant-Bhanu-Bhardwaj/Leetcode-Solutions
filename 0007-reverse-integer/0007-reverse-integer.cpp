7ab074d3a204948f73735321feb37ba13d4516c8
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2025-11-07 01:05:22 +0530

    Time: 2 ms (38.87%), Space: 8.6 MB (53.86%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int remain;
+        long long rev=0;
+        while(x!=0){
+            remain = x%10;
+            rev = rev*10 + remain;
+            x = x/10;
+        }
+
+    if (rev < INT_MIN || rev > INT_MAX) {
+            return 0;
+        }
+
+        return (int)rev;
+    }
+};
