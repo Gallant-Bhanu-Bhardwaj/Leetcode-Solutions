fe133fa6c7a18d0dc20200057589b1c8e4cb6d33
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-02 10:47:08 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.33%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xorAll = 0;
-        for(int i=0;i <nums.size();i++)
-        {
-            xorAll ^=  nums[i];
-        }
-        return xorAll;
+        int ans = 0;
+        for(int i : nums)
+         ans = ans ^ i;
+        return ans; 
     }
 };
