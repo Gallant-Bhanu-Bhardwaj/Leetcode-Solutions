bcd6cecb1714460a9618ae1c30e3dfa4b25197ca
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-02 18:28:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long binform = n;
+        long long binform = n;
         if(n < 0)
         {
             x = 1/x;
             binform = -binform;
         }
-        double ans=1;
+        double ans = 1.0;
         while(binform > 0)
         {
             if(binform & 1)
