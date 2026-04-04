3e91aaf27b154fbbd6cf759724db92febcb51754
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-05 03:12:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.cpp
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.cpp
@@ -5,10 +5,7 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             sum += nums[i];
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            while(nums[i]>0)
+             while(nums[i]>0)
             {
                 int rem = nums[i]%10;
                 num += rem;
