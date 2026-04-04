fed68e3419e63628ea6ad89883a11c451baf8c4b
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-05 02:42:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,7 +5,7 @@ public:
         int col = matrix[0].size();
         for(int i=0;i<row;i++)
         {
-            for(int j=i;j<col;j++)
+            for(int j=i+1;j<col;j++)
             {
                 swap(matrix[i][j],matrix[j][i]);
             }
