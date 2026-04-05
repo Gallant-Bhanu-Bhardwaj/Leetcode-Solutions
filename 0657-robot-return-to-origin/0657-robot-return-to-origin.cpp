15b28d4a8c52e47cc8bc13c10f125dd44be5f9a2
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-05 14:55:33 +0530

    Time: 1 ms (60.99%), Space: 10.3 MB (76.78%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int cntx = 0, cnty=0;
+        for(int i=0;i<moves.size();i++)
+        {
+            if(moves[i] == 'L')
+                cntx--;
+            else if(moves[i] == 'R')
+                cntx++;    
+            else if(moves[i] == 'U')
+                cnty++; 
+            else        
+                cnty--;
+        }
+        if(cntx==0 && cnty == 0)
+        return true;
+        else
+         return false;
+    }
+};
