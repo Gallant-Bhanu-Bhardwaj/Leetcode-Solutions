c436f8261334eed6cbd4a6746a4d952970fc7a0a
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-14 19:33:25 +0530

    Time: 0 ms (100%), Space: 7.9 MB (67.32%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = x ^ y, cnt = 0;
+        while (res > 0) {
+            if (res & 1 == 1)
+                cnt++;
+            res = res >> 1;
+        }
+        return cnt;
+    }
+};
