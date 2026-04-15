ae22fd941c645b616cf6236d8458829ac1eab216
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-15 08:07:02 +0530

    Time: 0 ms (100%), Space: 9.3 MB (49.25%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int cnt1 = 0 , cnt2 = 0, i=0,j=0;
-        while(i<s.size())
-        {
-            cnt1 += s[i]-'a'; 
-            i++;
-        }
-        while(j<t.size())
-        {
-            cnt2 += t[j]-'a';
-            j++;
-        }
-        if(s.size() > t.size())
-         return 'a' + cnt1-cnt2;
-        else
-         return 'a' + cnt2-cnt1;
+        char result = 0;
+        for(char c : s) result ^= c;
+        for(char d : t) result ^= d;
+        return result;
     }
 };
