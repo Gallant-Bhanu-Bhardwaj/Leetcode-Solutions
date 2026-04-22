ba93bda9a103ef81895b1f750bfe6eca61082768
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-22 18:18:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-bool isPrime(int n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-
-    if (n % 2 == 0) return false;  
-
-    for (int i = 3; i * i <= n; i += 2) {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
-
     int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes = {2,3,5,7,11,13,17,19,23,29,31};
         int count = 0;
-        for(int i=left ; i<= right; i++)
-        {
+
+        for (int i = left; i <= right; i++) {
             int bits = __builtin_popcount(i);
-            if(isPrime(bits))
-             count++;
+            if (primes.count(bits))
+                count++;
         }
+
         return count;
     }
 };
