0c0c67fc1859042c493320524d9beb857a825162
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-06-08 22:33:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.54%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+
+        return gcd(b, a % b);
+    }
     int gcdOfOddEvenSums(int n) {
         int sumodd = 0, sumeven = 0;
         int a1 = 1, d = 2;
-        sumodd = ((n) * (2*a1 + (n - 1) * d)) / 2;
+        sumodd = ((n) * (2 * a1 + (n - 1) * d)) / 2;
 
         int a2 = 2;
-        sumeven = ((n) * (2*a2 + (n - 1) * d)) / 2;
+        sumeven = ((n) * (2 * a2 + (n - 1) * d)) / 2;
 
         return gcd(sumodd, sumeven);
     }
