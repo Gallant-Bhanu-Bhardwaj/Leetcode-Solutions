f5826a4b0e11a6d79e166333a47a42911b18dd4a
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-06-08 10:45:56 +0530

    Time: 0 ms (100%), Space: 125.6 MB (77.75%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -10,48 +10,21 @@
  */
 class Solution {
 public:
-ListNode* rev(ListNode* head)
-{
-    ListNode* prev = NULL, *curr=head;
-    while(curr)
-    {
-        ListNode* nextptr = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = nextptr;
-    }
-    return prev;
-}
     ListNode* doubleIt(ListNode* head) {
-        if(!head) return NULL;
+        if (!head)
+            return NULL;
 
-        head = rev(head);
-        int carry = 0;
-        ListNode* temp = head, *ans = nullptr;
-        int num = temp->val*2 + carry;
-        carry = num / 10;
-        int rem = num % 10;
-        ListNode* ptr = new ListNode(rem);
-            ans = ptr;
-        temp = temp -> next;   
+        if (head->val >= 5)
+            head = new ListNode(0, head);
 
-        while(temp)
-        {
-            num = temp->val*2 + carry;
-            carry = num / 10;
-            rem = num % 10;
-            ListNode* p = new ListNode(rem);
-            ptr -> next = p;
-            ptr = ptr->next;
-            temp = temp -> next;
+        ListNode* curr = head;
+        while (curr) {
+            curr->val = (curr->val * 2) % 10;
+            if (curr->next && curr->next->val >= 5)
+                curr->val++;
+
+            curr = curr->next;
         }
-        if(carry == 1)
-        {
-            ListNode* q = new ListNode(carry);
-            ptr->next = q;
-            q->next = NULL;
-        }
-        ans = rev(ans);
-        return ans;
+        return head;
     }
 };
