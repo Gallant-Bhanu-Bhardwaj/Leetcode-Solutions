6c6270bed4aa550a66c6b790a3d87771ea09912b
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-08-20 11:29:41 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.63%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -4,15 +4,11 @@ public:
         int n = nums.size();
         vector<int> arr1;
         vector<int> arr2;
-
-        int i = 0;
-        arr1.push_back(nums[i]);
-        i++;
-        arr2.push_back(nums[i]);
-        i++;
-        for(; i<n;i++)
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        for(int i=2; i<n;i++)
         {
-            if(arr1[arr1.size()-1] > arr2[arr2.size()-1])
+            if(arr1.back() > arr2.back())
              arr1.push_back(nums[i]);
             else
              arr2.push_back(nums[i]);
